158fec72a5a682bfaa84cb6d0789ec3292cdfd28
Valeria <05valarist@gmail.com>
2025-04-04 01:18:59 -0500

    Entidad cliente creada y lista

--- parcial/src/main/java/com/example/parcial/model/Cliente.java
+++ parcial/src/main/java/com/example/parcial/model/Cliente.java
@@ -1,7 +1,82 @@
 package com.example.parcial.model;
-
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; //id de la tabla autoincrementable
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(unique = true, nullable = false)
+    private String cedula;
+
+    @Column(nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private String telefono;
+
+    @Column(nullable = false)
+    private int edad;
+
+    //constructor vacío para JPA
+    public Cliente(){
+
+    }
+
+    //constructor para inicializar Cliente
+    public Cliente(String nombre, String cedula, String correo, String telefono, int edad){
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    //get & set
+    public Integer getId() { 
+        return id; 
+    }
+    public void setId(Integer id) { 
+        this.id = id; 
+    }
+
+    public String getNombre() { 
+        return nombre; 
+    }
+
+    public void setNombre(String nombre) {
+         this.nombre = nombre; 
+    }
+
+    public String getCedula() { 
+        return cedula; 
+    }
+    public void setCedula(String cedula) { 
+        this.cedula = cedula; 
+    }
+
+    public String getTelefono() { 
+        return telefono; 
+    }
+    public void setTelefono(String telefono) { 
+        this.telefono = telefono; 
+    }
+
+    public String getCorreo() { 
+        return correo; 
+    }
+    public void setCorreo(String correo) { 
+        this.correo = correo; 
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
 }
